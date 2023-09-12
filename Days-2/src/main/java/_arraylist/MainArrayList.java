bec0e20ef32e3ffe431b7750923b2b6aa0128eb7
Hakan ÖZER <hakanozer02@gmail.com>
2023-09-12 12:59:56 +0300

    Days-2

---
+++ Days-2/src/main/java/_arraylist/MainArrayList.java
@@ -0,0 +1,95 @@
+package _arraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainArrayList {
+
+    public static void main(String[] args) {
+
+        String[] stArr = {"İstanbul", "Ankara", "Adana"};
+        stArr[2] = "Aydın";
+        System.out.println( stArr );
+
+
+        // ArrayList
+        List<String> ls = new ArrayList<>();
+
+        // add
+        ls.add("İstanbul");
+        ls.add("Ankara");
+        ls.add("İzmir");
+        ls.add("Samsun");
+        ls.add("Adana");
+        ls.add("Edirne");
+
+        // add index
+        ls.add(1, "Edirne");
+
+        // remove
+        ls.remove("Edirne");
+
+        ArrayList<String> removeList = new ArrayList<>();
+        removeList.add("Edirne");
+        removeList.add("İstanbul");
+        //ls.removeAll(removeList);
+
+        // size
+        int size = ls.size();
+        int index = 10;
+        if ( size > index ) {
+            ls.remove(index);
+        }
+
+        // all item clear
+        // ls.clear();
+
+        // loop
+        for ( String item : ls ) {
+            System.out.println( item );
+        }
+
+        String name1 = "Ali";
+        String name2 = "Alim";
+        System.out.println( name1.hashCode() );
+        System.out.println( name2.hashCode() );
+
+        // contains
+        boolean status = ls.contains("İstanbul");
+        System.out.println(status);
+
+        String itm = ls.get(0);
+        System.out.println(itm);
+
+        int indexOf = ls.indexOf("Edirne");
+        System.out.println( indexOf );
+
+        // Değer değişimi
+        ls.set(0, "Gaziantep");
+        ls.add(0, "İstanbul");
+
+        Iterator<String> iterator = ls.iterator();
+        while( iterator.hasNext() ) {
+            String item = iterator.next();
+            System.out.println("iter : " + item);
+        }
+
+        while( iterator.hasNext() ) {
+            String item = iterator.next();
+            System.out.println("iter x: " + item);
+        }
+
+        List<String> pageList = ls.subList(5, 7);
+        System.out.println(pageList);
+
+        // Sort
+        Collections.sort(ls);
+        System.out.println(ls);
+
+        Collections.reverse(ls);
+        System.out.println(ls);
+    }
+
+}
---
+++ Days-2/src/main/java/_arraylist/User.java
@@ -0,0 +1,33 @@
+package _arraylist;
+
+public class User {
+
+    private String name;
+    private String phone;
+    private Byte age; // 127 - 128
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+}
